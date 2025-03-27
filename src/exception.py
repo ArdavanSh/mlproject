5732baf7d47c997c221eb2c9f0668c41aeaa0c41
Ardavan Sherafat <75146210+ArdavanSh@users.noreply.github.com>
2025-03-26 19:02:09 -0700

    loggin and exception

---
+++ src/exception.py
@@ -0,0 +1,37 @@
+import sys
+from logger import logger
+
+def error_message_detail(error, error_detail: sys):
+    """
+    Extracts and formats the error message from an exception.
+    
+    Args:
+        error (Exception): The exception object.
+        error_detail (sys): The sys module to extract the traceback information.
+
+    Returns:
+        str: A formatted string containing the error message and details.
+    """
+    _, _, exc_tb = error_detail.exc_info()
+    file_name = exc_tb.tb_frame.f_code.co_filename
+    line_number = exc_tb.tb_lineno
+    error_message = f"Error occurred in script: [{file_name}] at line number: [{line_number}] with message: [{str(error)}]"
+    return error_message 
+
+
+class CustomException(Exception):
+    """
+    Custom exception class that formats error messages for exceptions.
+
+    Args:
+        error (Exception): The exception object.
+        error_detail (sys): The sys module to extract the traceback information.
+    """
+    def __init__(self, error, error_detail: sys):
+        super().__init__(error)
+        self.error_message = error_message_detail(error, error_detail)
+
+    def __str__(self):
+        return self.error_message
+    
+
---
+++ src/logger.py
@@ -0,0 +1,21 @@
+import logging
+import os
+from datetime import datetime
+
+# Create a log filename with the current date
+log_filename = f"{datetime.now().strftime('%Y-%m-%d')}.log"
+log_dir = os.path.join(os.getcwd(), "logs")
+os.makedirs(log_dir, exist_ok=True)
+
+log_file_path = os.path.join(log_dir, log_filename)
+
+# Set up logging configuration
+logging.basicConfig(
+    filename=log_file_path,
+    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO,
+)
+
+
+
+logger = logging.getLogger(__name__)
